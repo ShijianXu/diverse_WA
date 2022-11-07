ef2fcc0df7b9791bcdb12a44c2526f9b76118165
shijianxu <13260906215@163.com>
2022-11-07 16:40:29 +0100

    add single model test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 python3 -m domainbed.scripts.test_single \
        --data_dir=../data \
-       --model_path=./model_path \
+       --model_path=./model_best.pkl \
        --dataset PACS \
        --test_env 0
