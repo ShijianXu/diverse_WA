2616a4f3da770a38600ffd46abb9b57c5028f111
shijianxu <13260906215@163.com>
2022-11-17 18:15:32 +0100

    add CIFAR100 test support

--- domainbed/scripts/test_single.py
+++ domainbed/scripts/test_single.py
@@ -1,5 +1,14 @@
 ## code used to load and test a single model
 
+"""
+python3 -m domainbed.scripts.test_single \
+       --data_dir=../data \
+       --model_path=./model_best.pkl \
+       --dataset CIFAR100 \
+       --test_env 0
+"""
+
+
 import argparse
 import os
 import json
@@ -7,6 +16,9 @@ import random
 import numpy as np
 import torch
 import torch.utils.data
+import torchvision
+from torchvision import transforms
+
 from domainbed import datasets, algorithms_inference
 from domainbed.lib import misc
 from domainbed.lib.fast_data_loader import FastDataLoader
@@ -52,22 +64,33 @@ def main():
     if inf_args.dataset in vars(datasets):
         dataset_class = vars(datasets)[inf_args.dataset]
         dataset = dataset_class(inf_args.data_dir, [inf_args.test_env], hparams={"data_augmentation": False})
+    elif inf_args.dataset == 'CIFAR100':
+        test_transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+            mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        dataset = torchvision.datasets.CIFAR100(root="../data", train=False, download=True, transform=test_transform)
     else:
         raise NotImplementedError
 
 
     # load test domain data
-    data_splits, data_names = [], []
-    dict_domian_to_filter = {"test": "full"}
+    if inf_args.dataset in vars(datasets):
+        data_splits, data_names = [], []
+        dict_domian_to_filter = {"test": "full"}
 
-    for domain in dict_domian_to_filter:
-        _data_splits = create_splits(domain, inf_args, dataset, dict_domian_to_filter[domain])
-        if domain == "train":
-            raise NotImplementedError
-        else:
-            data_splits.append(_data_splits[0])
+        for domain in dict_domian_to_filter:
+            _data_splits = create_splits(domain, inf_args, dataset, dict_domian_to_filter[domain])
+            if domain == "train":
+                raise NotImplementedError
+            else:
+                data_splits.append(_data_splits[0])
 
-        data_names.append(domain)
+            data_names.append(domain)
+    
 
     # start testing
     save_dict = torch.load(inf_args.model_path, map_location=torch.device(device))
@@ -89,16 +112,24 @@ def main():
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    data_loaders = [
-        FastDataLoader(dataset=split, batch_size=64, num_workers=dataset.N_WORKERS)
-        for split in data_splits
-    ]
+    if inf_args.dataset in vars(datasets):
+        data_loaders = [
+            FastDataLoader(dataset=split, batch_size=64, num_workers=dataset.N_WORKERS)
+            for split in data_splits
+        ]
+    
+        data_evals = zip(data_names, data_loaders)
+        
+        for name, loader in data_evals:
+            print(f"Inference at {name}")
+            acc = misc.accuracy(algorithm, loader, None, device)
 
-    data_evals = zip(data_names, data_loaders)
-
-    for name, loader in data_evals:
-        print(f"Inference at {name}")
-        acc = misc.accuracy(algorithm, loader, None, device)
+    else:
+        eval_loader = FastDataLoader(
+            dataset=dataset,
+            batch_size=64,
+            num_workers=4)
+        acc = misc.accuracy(algorithm, eval_loader, None, device)
 
     print(f"Test accuracy: {acc}")
 
