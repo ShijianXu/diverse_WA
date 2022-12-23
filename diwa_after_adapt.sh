2f2c17e530765dba03e8755e9f1b3d4084a874ed
shijianxu <13260906215@163.com>
2022-12-23 23:04:51 +0100

    small modification

--- diwa_after_adapt.sh
+++ diwa_after_adapt.sh
@@ -1,10 +1,9 @@
 python3 -m domainbed.scripts.few_shot_adapt_before_WA \
     --data_dir=../data \
     --target_dataset SVHN \
-    --sweep_dir=./mnist_res18_sweep_diwa_adam \
-    --output_dir=./mnist_res18_adam_diwa_2_svhn_adam_10_shot \
+    --sweep_dir=./mnist_res18_sweep_diwa_sam \
     --weight_selection uniform \
-    --opt_name Adam \
+    --opt_name SAM \
     --sam_rho 0.05 \
     --k_shot 10 \
-    --steps 400
+    --steps 100
--- domainbed/scripts/few_shot_adapt_before_WA.py
+++ domainbed/scripts/few_shot_adapt_before_WA.py
@@ -1,4 +1,14 @@
-
+"""
+python3 -m domainbed.scripts.few_shot_adapt_before_WA \
+    --data_dir=../data \
+    --target_dataset SVHN \
+    --sweep_dir=./mnist_res18_sweep_diwa_sam \
+    --weight_selection uniform \
+    --opt_name SAM \
+    --sam_rho 0.05 \
+    --k_shot 10 \
+    --steps 100
+"""
 
 import argparse
 import os
