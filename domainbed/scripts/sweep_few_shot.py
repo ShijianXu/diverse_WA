3d1306abfb11cb96a597ce75053bbaafb6e99280
shijianxu <13260906215@163.com>
2022-12-30 10:44:19 +0100

    modify sweep_few_shot.py for VisDA sweep train

--- domainbed/scripts/sweep_few_shot.py
+++ domainbed/scripts/sweep_few_shot.py
@@ -1,12 +1,28 @@
 """
 # Diverse weight averaging training for MNIST
-python3 -m domainbed.scripts.sweep_mnist launch \
+python3 -m domainbed.scripts.sweep_few_shot launch \
     --data_dir=../data \
     --output_dir=./MNIST_sweep_diwa_adam \
     --path_for_init ./MNIST_adam_pretrain.pth \
     --command_launcher local \
     --opt_name Adam \
     --steps 1000 \
+    --check_freq 1000 \
+    --n_hparams 10 \
+    --n_trials 1 \
+    --skip_confirmation
+
+
+python3 -m domainbed.scripts.sweep_few_shot launch \
+    --data_dir=../data \
+    --train_data VisDA \
+    --num_classes 12 \
+    --output_dir=./VisDA_sweep_diwa_sam \
+    --path_for_init ./res50_visda_lineprobe_sam_future_init.pth \
+    --command_launcher local \
+    --opt_name SAM \
+    --model_name resnet50 \
+    --steps 10000 \
     --check_freq 1000 \
     --n_hparams 10 \
     --n_trials 1 \
@@ -45,7 +61,7 @@ class Job:
 
         self.train_args = copy.deepcopy(train_args)
         self.train_args['output_dir'] = self.output_dir
-        command = ['python', '-m', 'domainbed.scripts.few_shot_mnist_train']
+        command = ['python', '-m', 'domainbed.scripts.few_shot_train']
         for k, v in sorted(self.train_args.items()):
             if isinstance(v, list):
                 v = ' '.join([str(v_) for v_ in v])
@@ -89,7 +105,7 @@ class Job:
         print(f'Deleted {len(jobs)} jobs!')
 
 
-def make_args_list(n_trials, opt_name, n_hparams_from, n_hparams, steps, data_dir, hparams, path_for_init, check_freq):
+def make_args_list(n_trials, opt_name, n_hparams_from, n_hparams, steps, data_dir, train_data, num_classes, hparams, path_for_init, model_name, check_freq):
     args_list = []
     for trial_seed in range(n_trials):
         for hparams_seed in range(n_hparams_from, n_hparams):
@@ -97,8 +113,11 @@ def make_args_list(n_trials, opt_name, n_hparams_from, n_hparams, steps, data_di
             train_args['opt_name'] = opt_name
             train_args['hparams_seed'] = hparams_seed
             train_args['data_dir'] = data_dir
+            train_args['train_data'] = train_data
+            train_args['num_classes'] = num_classes
             train_args['trial_seed'] = trial_seed
             train_args['path_for_init'] = path_for_init
+            train_args['model_name'] = model_name
             train_args['seed'] = misc.seed_hash(opt_name, hparams_seed, trial_seed)
             train_args['check_freq'] = check_freq
 
@@ -123,6 +142,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run DiWA sweep for MNIST')
     parser.add_argument('command', choices=['launch', 'delete_incomplete'])
     parser.add_argument('--data_dir', type=str)
+    parser.add_argument('--train_data', type=str, default='MNIST')
+    parser.add_argument('--num_classes', type=int, default=10)
     parser.add_argument('--opt_name', type=str, default="Adam")
     parser.add_argument('--steps', type=int, default=10000)
     parser.add_argument('--check_freq', type=int, default=1000)
@@ -135,7 +156,8 @@ if __name__ == "__main__":
     parser.add_argument('--hparams', type=str, default=None)
     parser.add_argument('--skip_confirmation', action='store_true')
     parser.add_argument('--path_for_init', type=str, default=None)
-
+    parser.add_argument('--model_name', type=str, default="resnet18")
+    
     args = parser.parse_args()
 
     args_list = make_args_list(
@@ -145,8 +167,11 @@ if __name__ == "__main__":
         n_hparams=args.n_hparams,
         steps=args.steps,
         data_dir=args.data_dir,
+        train_data=args.train_data,
+        num_classes=args.num_classes,
         hparams=args.hparams,
         path_for_init=args.path_for_init,
+        model_name=args.model_name,
         check_freq=args.check_freq
     )
 
