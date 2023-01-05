c2c9b3212543952dd9a026f5cc6d1af49fd8f01e
shijianxu <13260906215@163.com>
2023-01-05 15:56:44 +0100

    delete a script file

--- mnist_train.sh
+++
@@ -1,8 +0,0 @@
-python3 -m domainbed.scripts.few_shot_mnist_train \
-    --data_dir=../data \
-    --opt_name SAM \
-    --output_dir=./mnist_init_train_sam \
-    --path_for_init ./mnist_future_init_sam.pth \
-    --steps 100 \
-    --check_freq 100 \
-    --init_step
