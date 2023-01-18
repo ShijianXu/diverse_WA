97c2c270644e1c735130cb664b11ec1448a678cd
shijianxu <13260906215@163.com>
2023-01-18 11:14:00 +0100

    train_id.sh

--- train_id.sh
+++ train_id.sh
@@ -1,8 +1,8 @@
 python3 -m domainbed.scripts.train_id \
        --data_dir=../data \
-       --output_dir=./CIFAR100_pretrain \
-       --algorithm ERM \
+       --output_dir=./CIFAR100_pretrain_test \
+       --algorithm SAM \
+       --sam_rho 0.05 \
        --checkpoint_freq 100 \
        --init_step \
-       --path_for_init ./CIFAR100_init.pth \
-       --steps 0
+       --path_for_init /scratch/izar/sxu/CIFAR100_init_sam_test.pth
