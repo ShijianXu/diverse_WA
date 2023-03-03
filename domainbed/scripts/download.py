796e728d66c4d97baefca79b2abdba02decaa6c7
shijianxu <13260906215@163.com>
2023-03-04 00:49:31 +0100

    data download

--- domainbed/scripts/download.py
+++ domainbed/scripts/download.py
@@ -129,7 +129,9 @@ def download_office_home(data_dir):
     # Original URL: http://hemanthdv.org/OfficeHome-Dataset/
     full_path = stage_path(data_dir, "office_home")
 
-    download_and_extract("https://drive.google.com/uc?id=1uY0pj7oFsjMxRwaD3Sxy0jgel0fsYXLC",
+    # download_and_extract("https://drive.google.com/uc?id=1uY0pj7oFsjMxRwaD3Sxy0jgel0fsYXLC",
+    #                      os.path.join(data_dir, "office_home.zip"))
+    download_and_extract("https://drive.google.com/file/d/0B81rNlvomiwed0V1YUxQdC1uOTg/view?usp=sharing&resourcekey=0-2SNWq0CDAuWOBRRBL7ZZsw",
                          os.path.join(data_dir, "office_home.zip"))
 
     os.rename(os.path.join(data_dir, "OfficeHomeDataset_10072016"),
